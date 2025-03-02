21a7b5d86810473249e428ca81f3c916705ab04a
mazlanatampan <datamazlana@gmail.com>
2025-03-02 22:57:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make
+
+
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u PEPE:0x78EEBB5096dC04014908Bb15B57A691A0378e28A.unmineable_worker_yeesqa -p x 
