f36ba6511047a560b6fab662b76531ef7ad3af62
mazlanatampan <datamazlana@gmail.com>
2025-03-05 01:09:56 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u PEPE:0x78EEBB5096dC04014908Bb15B57A691A0378e28A.unmineable_worker_yeesqa -p x 
+cd /xmrig
+cd /build
+
+./xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 -u PEPE:0x78EEBB5096dC04014908Bb15B57A691A0378e28A.mazlana -p x 
